932b94e6d9ca40af7b98c4e69472eb19757feb6b
ysu519 <90619322+ysu519@users.noreply.github.com>
2022-07-04 13:48:55 +0900

    인게임씬 UI배치

--- My project/Assets/00.ProjectOzFolder/02.Scripts/97.ButtonUI/MenuButton.cs
+++ My project/Assets/00.ProjectOzFolder/02.Scripts/97.ButtonUI/MenuButton.cs
@@ -1,23 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButton : BasedButtonUIScrept
 {
     GameObject UsingTarget;
     public void Button_NewGame()
     {
-
+        //StartCoroutine(LoadingScene());
     }
 
     public void Button_Continue()
     {
 
-    }
-    public void Button_Setting()
-    {
-        Target[1].SetActive(true);
-        UsingTarget = Target[1];
     }
     public void Button_GameOver()
     {
@@ -35,34 +31,58 @@ public class MenuButton : BasedButtonUIScrept
         }
     }
 
-    public void Button_VideoWindowOn()
+    public void Button_WindowOn_Option()
+    {
+        Target[1].SetActive(true);
+        UsingTarget = Target[1];
+    }
+    public void Button_WindowOn_Video()
     {
         UsingTarget.SetActive(false);
         Target[2].SetActive(true);
         UsingTarget = Target[2];
     }
-    public void Button_AudioWindowOn()
+    public void Button_WindowOn_Audio()
     {
         UsingTarget.SetActive(false);
         Target[3].SetActive(true);
         UsingTarget = Target[3];
     }
-    public void Button_KeyWindowOn()
+    public void Button_WindowOn_Key()
     {
         UsingTarget.SetActive(false);
         Target[4].SetActive(true);
         UsingTarget = Target[4];
     }
-    public void Button_AchievementsWindowOn()
+    public void Button_WindowOn_Achievements()
     {
         UsingTarget.SetActive(false);
         Target[5].SetActive(true);
         UsingTarget = Target[5];
     }
-    public void Button_CreditWindowOn()
+    public void Button_WindowOn_Credit()
     {
         UsingTarget.SetActive(false);
         Target[6].SetActive(true);
         UsingTarget = Target[6];
+    }
+
+    public void Button_Keep()
+    {
+
+    }
+
+    public void Button_MainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+        //메인 메뉴로 갈땐 로딩 필요 없을거 같아서 그냥 로드씬 사용했습니다.
+    }
+
+    IEnumerator LoadingScene()
+    {
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync("InGameScene01");
+        //로딩화면 등 추후 추가
+        while(!asyncload.isDone)
+            yield return null;
     }
 }
